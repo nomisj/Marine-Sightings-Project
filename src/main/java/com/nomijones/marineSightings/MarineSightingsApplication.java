1d76567f347741005f93f564e52585e296974c8f
nomisj <nomij5941@gmail.com>
2022-08-03 20:36:45 -0700

    Solo Project Marine Sightings

---
+++ src/main/java/com/nomijones/marineSightings/MarineSightingsApplication.java
@@ -0,0 +1,13 @@
+package com.nomijones.marineSightings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarineSightingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarineSightingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nomijones/marineSightings/MarineSightingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nomijones.marineSightings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarineSightingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
